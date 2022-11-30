df8fb00b46f90b05a10999e8048ee9129b0b3f35
JupiterIvy <evymafra11@gmail.com>
2022-11-29 22:32:53 -0400

    commit to github

---
+++ test.py
@@ -0,0 +1,10 @@
+lists = {'id': 1, 
+'name': 'Musashi Jessica', 
+'role': 'Químico',
+ 'email': 'jessie@pokemon.com', 
+ 'password': '1234', 
+ 'image_link': 'https://i.pinimg.com/474x/cb/bb/b1/cbbbb1a98bf9770064ac5475a3f5a802.jpg'
+}
+
+for i in lists:
+    print(lists[i])
